bdee7e4ba27675e0982a0b696893d7908c8cc32c
ppechtue <66050252@kmitl.ac.th>
2026-02-11 20:57:03 +0700

    Add files via upload

---
+++ cell04/ex05/up_low.py
@@ -0,0 +1,12 @@
+text = input("input:")
+result = ""
+
+for ch in text:
+    if ch.isupper():
+        result += ch.lower()
+    elif ch.islower():
+        result += ch.upper()
+    else:
+        result += ch
+
+print(result)

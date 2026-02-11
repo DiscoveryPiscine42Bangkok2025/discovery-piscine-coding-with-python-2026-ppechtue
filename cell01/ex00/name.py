228b6c83dfd0791bf8e9b7b786721120b7699b5e
ppechtue <66050252@kmitl.ac.th>
2026-02-11 20:55:47 +0700

    Add files via upload

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Patipat"
+last_name = "Pechtuek"
+print(first_name,last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Patipat"
+last_name = "Patipat"
+whole_name = first_name + " " + last_name
+print(whole_name)
